c6ee28a12cb3f8e4826f06e6334670bc77c51189
wangshiz <312406078@qq.com>
2019-03-18 18:37:09 +0800

    123

--- cloudfunctions/insertrecord/index.js
+++ cloudfunctions/insertrecord/index.js
@@ -21,29 +21,28 @@ exports.main = async (event, context) => {
   }
 
   try {
-    await db.collection('busi_sign_in_record').add({
-      data: {
-        signid: event.signid,
-        date: dbtime,
-        year: dateYear.toString(),
-        month: dateMonth >= 10? dateMonth.toString(): "0"+dateMonth.toString(),
-        day: dateDay >= 10 ? dateDay.toString() : "0" + dateDay.toString(),
-      }
-    })
-    
-    const res = await cloud.callFunction({
-      // 要调用的云函数名称
-      name: 'insertrecord',
-      // 传递给云函数的参数
-      data: {
-        shouldAdd: shouldAdd,
-        lastSignDate: dbtime,
-        signid: event.signid
-      }
-    })
-    return res.result
-    
+    // await db.collection('busi_sign_in_record').add({
+    //   data: {
+    //     signid: event.signid,
+    //     date: dbtime,
+    //     year: dateYear.toString(),
+    //     month: dateMonth >= 10? dateMonth.toString(): "0"+dateMonth.toString(),
+    //     day: dateDay >= 10 ? dateDay.toString() : "0" + dateDay.toString(),
+    //   }
+    // })
 
+    await db.collection('busi_sign_in').doc(event.signid).update({
+      data: {
+        last_sign_date: date,
+        day_count: _.inc(2),
+        cont_count: shouldAdd ? _.inc(2) : _.inc(1)
+      }
+    })
+ 
+    
+    return {
+      success
+    }
   } catch (e) {
     console.log(e)
   }
